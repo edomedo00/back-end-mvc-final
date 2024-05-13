706926c69ec1a2ac9842b7003d93dc9e4aecd8d2
EdmundoMedelDev <edramede@gmail.com>
2024-05-13 09:52:47 -0600

    Project initialization - Server running TRUE

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+// const authRoutes = require ('./routes/authRoutes')
+const app = express()
+const PORT = process.env.PORT || 6010
+
+app.use(cors())
+app.use(express.json())
+
+// app.use('/api/auth', authRoutes)
+
+app.listen(PORT, ()=>{
+  console.log(`Server running on port: ${PORT}`)
+})
