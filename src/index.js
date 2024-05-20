d58464a86dd65d0b2e040390b036237692b1c283
EdmundoMedelDev <edramede@gmail.com>
2024-05-20 10:09:46 -0600

    environment variables detection fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+require('dotenv').config({ path: __dirname + '/.env' });
 const express = require('express')
 const cors = require('cors')
-require('dotenv').config()
 const authRoutes = require('./routes/authRoutes')
 const app = express()
 const PORT = process.env.PORT || 6010
